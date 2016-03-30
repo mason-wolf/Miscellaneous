76f1c19e38164e5978884a73047b104b0a4ccccd
mason-wolf <masonhwolf@gmail.com>
2016-03-30 13:09:02 -0500

    Create elements.cs

---
+++ cs/elements.cs
@@ -0,0 +1,14 @@
+using System;
+
+class elements {
+	static void Main() {
+		
+		string[] elements = new string[] {"fire", "earth", "water"};
+		
+		foreach (string element in elements) {
+			Console.WriteLine(element);
+		}
+		
+		Console.ReadLine();
+	}
+}
