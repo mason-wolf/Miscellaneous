4ecae5e38e67d6e5f603538455837c083749281b
mason <mason@wolfden>
2016-08-22 02:59:33 -0500

    libcurl example added

---
+++ c/curlfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <stdlib.h>
+
+// https://curl.haxx.se/libcurl/c/
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
+int main(void) {
+
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+
+    char *url = "https://mail.google.com/mail/feed/atom";
+    char outfilename[FILENAME_MAX] = "feed.txt";
+    curl = curl_easy_init();
+    if (curl) {
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_USERPWD, "");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
+    return 0;
+}
