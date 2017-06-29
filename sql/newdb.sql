060201597219a3e75ea07fa106165bef806b81d8
Mason Wolf <masonhwolf@gmail.com>
2017-06-29 18:37:02 -0500

    Create newdb.sql

---
+++ sql/newdb.sql
@@ -0,0 +1,9 @@
+create table users (
+	ID int auto_increment primary key,
+	FirstName char(255),
+	LastName char(255),
+	Username char(255),
+	Password char(255),
+	AssignedKit char(255),
+	InspectionDate char(255)
+);
